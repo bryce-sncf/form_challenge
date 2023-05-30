1e84f7ec8d45422f01e4b5276bfc0320f398a9df
bryce-sncf <bryce.marcol@sncf.fr>
2023-05-30 13:56:28 +0200

    form ok

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './user';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+  @ViewChild('signupForm') signupForm!: NgForm;
+  user: User = new User();
 
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.user);
+    }
+  }
 }
